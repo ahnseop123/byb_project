bd708783e3a68ad3d60e13ccd45e3773faa130a2
ahnseop123 <135173868+ahnseop123@users.noreply.github.com>
2023-06-21 21:40:21 +0100

    helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,14 @@
+# Take in user's name using input()
+name = input("Please enter your name: ")
+
+# Print out the user's name
+print("Your name is:", name)
+
+# Take in user's age using input()
+age = input("Please enter your age: ")
+
+# Print out the user's age
+print("Your age is:", age)
+
+# Print "Hello World!" on a new line
+print("Hello World!")
